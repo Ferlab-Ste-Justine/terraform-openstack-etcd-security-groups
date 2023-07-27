e6fd806706b9559b6a1c619a558f376f30f6b8b0
Eric Vallee <eric_vallee@webificservices.com>
2023-07-27 16:40:39 -0400

    fix: INFRA-403 Fixed non-determinism of map in security groups rules creation

--- security_groups.tf
+++ security_groups.tf
@@ -30,101 +30,101 @@ resource "openstack_networking_secgroup_rule_v2" "peer_etcd_access" {
 
 //Allow port 22 and icmp traffic from the bastion
 resource "openstack_networking_secgroup_rule_v2" "internal_ssh_access" {
-  for_each          = { for id in var.bastion_group_ids : id => id }
+  for_each          = { for idx, id in var.bastion_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v4" {
-  for_each          = { for id in var.bastion_group_ids : id => id }
+  for_each          = { for idx, id in var.bastion_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v6" {
-  for_each          = { for id in var.bastion_group_ids : id => id }
+  for_each          = { for idx, id in var.bastion_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv6"
   protocol          = "ipv6-icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 //Allow port 2379 and icmp traffic from the client
 resource "openstack_networking_secgroup_rule_v2" "client_etcd_access" {
-  for_each          = { for id in var.client_group_ids : id => id }
+  for_each          = { for idx, id in var.client_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 2379
   port_range_max    = 2379
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "client_icmp_access_v4" {
-  for_each          = { for id in var.client_group_ids : id => id }
+  for_each          = { for idx, id in var.client_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "client_icmp_access_v6" {
-  for_each          = { for id in var.client_group_ids : id => id }
+  for_each          = { for idx, id in var.client_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv6"
   protocol          = "ipv6-icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 //Allow port 2379, 9100 and icmp traffic from metrics server
 resource "openstack_networking_secgroup_rule_v2" "metrics_server_node_exporter_access" {
-  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  for_each          = { for idx, id in var.metrics_server_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 9100
   port_range_max    = 9100
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "metrics_server_etcd_exporter_access" {
-  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  for_each          = { for idx, id in var.metrics_server_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 2379
   port_range_max    = 2379
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "metrics_server_icmp_access_v4" {
-  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  for_each          = { for idx, id in var.metrics_server_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "metrics_server_icmp_access_v6" {
-  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  for_each          = { for idx, id in var.metrics_server_group_ids : idx => id }
   direction         = "ingress"
   ethertype         = "IPv6"
   protocol          = "ipv6-icmp"
-  remote_group_id   = each.key
+  remote_group_id   = each.value
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
