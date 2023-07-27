db8226591aa95fe0f1869435ababfac307113fad
Eric Vallee <eric_vallee@webificservices.com>
2023-07-27 16:09:53 -0400

    feat: INFRA-403 Refactored module to better support networking that is upstream of the module declaration, but should be given access

--- security_groups.tf
+++ security_groups.tf
@@ -1,22 +1,10 @@
 resource "openstack_networking_secgroup_v2" "etcd_member" {
-  name                 = "${var.namespace}-etcd-member"
+  name                 = var.member_group_name
   description          = "Security group for etcd members"
   delete_default_rules = true
 }
 
-resource "openstack_networking_secgroup_v2" "etcd_client" {
-  name                 = "${var.namespace}-etcd-client"
-  description          = "Security group for the clients connecting to etcd members"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "etcd_bastion" {
-  name                 = "${var.namespace}-etcd-bastion"
-  description          = "Security group for the bastion connecting to etcd members"
-  delete_default_rules = true
-}
-
-//Allow all outbound traffic from etcd members and bastion
+//Allow all outbound traffic from etcd members
 resource "openstack_networking_secgroup_rule_v2" "etcd_member_outgoing_v4" {
   direction         = "egress"
   ethertype         = "IPv4"
@@ -27,18 +15,6 @@ resource "openstack_networking_secgroup_rule_v2" "etcd_member_outgoing_v6" {
   direction         = "egress"
   ethertype         = "IPv6"
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "etcd_bastion_outgoing_v4" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.etcd_bastion.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "etcd_bastion_outgoing_v6" {
-  direction         = "egress"
-  ethertype         = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.etcd_bastion.id
 }
 
 //Allow port 2380 traffic from other members
@@ -52,76 +28,103 @@ resource "openstack_networking_secgroup_rule_v2" "peer_etcd_access" {
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-//Allow port 22 traffic from the bastion
+//Allow port 22 and icmp traffic from the bastion
 resource "openstack_networking_secgroup_rule_v2" "internal_ssh_access" {
+  for_each          = { for id in var.bastion_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_bastion.id
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-//Allow port 22 traffic on the bastion
-resource "openstack_networking_secgroup_rule_v2" "external_ssh_access" {
+resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v4" {
+  for_each          = { for id in var.bastion_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.etcd_bastion.id
+  protocol          = "icmp"
+  remote_group_id   = each.key
+  security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-//Allow port 2379 traffic from the client
+resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v6" {
+  for_each          = { for id in var.bastion_group_ids : id => id }
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "ipv6-icmp"
+  remote_group_id   = each.key
+  security_group_id = openstack_networking_secgroup_v2.etcd_member.id
+}
+
+//Allow port 2379 and icmp traffic from the client
 resource "openstack_networking_secgroup_rule_v2" "client_etcd_access" {
+  for_each          = { for id in var.client_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 2379
   port_range_max    = 2379
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_client.id
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-//Allow clients and bastion to use icmp
 resource "openstack_networking_secgroup_rule_v2" "client_icmp_access_v4" {
+  for_each          = { for id in var.client_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_client.id
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "client_icmp_access_v6" {
+  for_each          = { for id in var.client_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv6"
-  protocol          = "icmp"
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_client.id
+  protocol          = "ipv6-icmp"
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v4" {
+//Allow port 2379, 9100 and icmp traffic from metrics server
+resource "openstack_networking_secgroup_rule_v2" "metrics_server_node_exporter_access" {
+  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 9100
+  port_range_max    = 9100
+  remote_group_id   = each.key
+  security_group_id = openstack_networking_secgroup_v2.etcd_member.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "metrics_server_etcd_exporter_access" {
+  for_each          = { for id in var.metrics_server_group_ids : id => id }
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 2379
+  port_range_max    = 2379
+  remote_group_id   = each.key
+  security_group_id = openstack_networking_secgroup_v2.etcd_member.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "metrics_server_icmp_access_v4" {
+  for_each          = { for id in var.metrics_server_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_bastion.id
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "bastion_icmp_access_v6" {
+resource "openstack_networking_secgroup_rule_v2" "metrics_server_icmp_access_v6" {
+  for_each          = { for id in var.metrics_server_group_ids : id => id }
   direction         = "ingress"
   ethertype         = "IPv6"
-  protocol          = "icmp"
-  remote_group_id  = openstack_networking_secgroup_v2.etcd_bastion.id
+  protocol          = "ipv6-icmp"
+  remote_group_id   = each.key
   security_group_id = openstack_networking_secgroup_v2.etcd_member.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "bastion_external_icmp_access" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.etcd_bastion.id
 }
